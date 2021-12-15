3372105f8f2a0e4763039c5f1a1da6bcbf7b9e48
Nate <nat233947@gmail.com>
2021-12-15 12:51:50 -0500

    working on Player class

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+const [canvasWidth, canvasHeight] = [1000, 400];
+let [xPos, yPos, player] = [0, 0];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
+  // player = new Player();
 }
 
 function draw() {
   background(220);
+  // player.draw();
+  rect(0, 0, 20, 20);
 }
