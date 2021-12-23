f22c03db1e61a873ced53d4ca51309549b94d719
Nate <nat233947@gmail.com>
2021-12-22 21:21:57 -0500

    Initial implementation of git commands

--- classes/Terminal.js
+++ classes/Terminal.js
@@ -2,18 +2,19 @@ class Terminal {
   constructor(dataJson) {
     // requiredData contains tha translated json file into json object
     this.requiredData = dataJson[0];
+    this.solution = dataJson[0].solution;
 
     // files contains the array of files from the dataJson
     // url contains the url of the folder from the dataJson
     // currDir contains the files of the current directory; will change
     // when cd is used and stuff
-    [this.files, this.url, this.currDir] = [
-      dataJson[0].files,
-      dataJson[0].url,
-      dataJson[0].files,
-    ];
+    this.files = dataJson[0].files;
+    this.url = dataJson[0].url;
+    this.currDir = dataJson[0].files;
+
+    // TODO "repo" to push to
     this.toPush = [];
-    console.log(this.files);
+    this.commitMsg = "";
   }
 
   parse(c) {
@@ -23,7 +24,7 @@ class Terminal {
 
     switch (cmd[1]) {
       case "git":
-        this.git(...cmd);
+        this.git(c);
         break;
       case "cd":
         if (cmd.length > 3)
@@ -55,13 +56,90 @@ class Terminal {
 
   git(c) {
     let cmd = c.split(" ");
-    switch (cmd[3]) {
+    console.log(cmd);
+    switch (cmd[2]) {
       case "add":
+        // Only git adds when there's at least one file listed
+        if (cmd.length >= 4) {
+          for (let i = 3; i < cmd.length; i++) {
+            let tobeadded = cmd[i];
+            this.currDir.forEach((file) => {
+              let currFileName = Object.keys(file)[0];
+              if (
+                currFileName === tobeadded ||
+                tobeadded === "." ||
+                tobeadded === "./"
+              ) {
+                computer.display += `${
+                  currFileName.includes(".") ? "File" : "Folder"
+                } ${currFileName} is now tracked!\n`;
+                this.push = this.toPush.push(file);
+              }
+            });
+
+            if (tobeadded === "." || tobeadded === "./") break;
+          }
+        }
         break;
       case "push":
+        if (this.toPush.length === 0)
+          computer.display += "No files are tracked!\n";
+
+        if (this.commitMsg.length === 0)
+          computer.display += "There is no commit message attached\n";
+
+        if (this.toPush.length === 0 || this.commitMsg.length === 0) return;
+
+        // does check to see if repo is the same as the given answers
+        let correctSolutionCheck = true;
+        console.log(this.toPush);
+        console.log(this.solution);
+        if (this.toPush.length !== this.solution.length)
+          correctSolutionCheck = false;
+        this.toPush.forEach((file) => {
+          let currFileName = Object.keys(file)[0];
+          let containsCurrentFile = false;
+          for (let i = 0; i < this.solution.length; i++) {
+            let currFileNameRepo = Object.keys(this.solution[i])[0];
+            if (currFileName === currFileNameRepo) containsCurrentFile = true;
+          }
+
+          if (!containsCurrentFile) correctSolutionCheck = false;
+        });
+
+        if (correctSolutionCheck)
+          computer.display += `Files tracked are pushed with commit message "${this.commitMsg}"!\n`;
+        else
+          computer.display += `Required files are not added to repo! Use "git stash" to start over! \n`;
         break;
       case "commit":
+        if (this.toPush.length === 0) {
+          computer.display +=
+            "There is no files tracked to attach commit message to!\n";
+          return;
+        }
+
+        let commitMsg = c.split('"');
+
+        if (commitMsg.length === 3) {
+          this.commitMsg = commitMsg[1];
+          computer.display += `Files are tracked with commit message "${this.commitMsg}"\n`;
+        } else
+          computer.display +=
+            "Error, be sure to encase commit message in double quotes\n";
         break;
+      case "ls-files":
+        this.toPush.forEach((file) => {
+          let currFileName = Object.keys(file)[0];
+
+          computer.display += `${
+            currFileName.includes(".") ? "File" : "Folder"
+          } ${currFileName} is tracked!\n`;
+        });
+        break;
+      case "stash":
+        this.toPush = [];
+        this.commitMsg = "";
       default:
         computer.display += "No such cmd found! :<\n";
         break;
@@ -70,11 +148,13 @@ class Terminal {
 
   cd(folder) {
     if (folder === "..") {
+      // checks to see if currently at base directory
       if (directoryPath.length == 1) {
         computer.display += "You're already at the base directory\n";
         return;
       }
 
+      // if cd refers to base dir, then set currDir to the files of base dir
       this.currDir = this.files;
 
       computer.display += "Back to base directory!\n";
