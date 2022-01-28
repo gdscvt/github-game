518896a23f772100dc5970668e0c6cd6d9ef88a8
Sarang-R-119 <sarangvr@yahoo.com>
2022-01-28 12:48:28 -0500

    Created level.js

---
+++ classes/Level.js
@@ -0,0 +1,34 @@
+class Level{
+    constructor(){
+
+    }
+
+    draw(){
+
+    }
+
+    setupProps(){
+        this.initTilemap(this.tilemap);
+        this.initTilemap(this.objectTilemap);
+    }
+
+    initTilemap(map) {
+        for (let i = 0; i < map.length; i++) {
+          for (let j = 0; j < map[i].length; j++) {
+            switch (map[i][j]) {
+              case "w":
+                this.walls.push(new Wall(envCapture[2], j * 20, i * 20));
+                break;
+      
+              case "t":
+                this.tiles.push(new Tile(envCapture[1], j * 20, i * 20));
+                break;
+      
+              case "d":
+                this.doors = new Door(doors, j * 20, i * 20, 20, 20);
+                break;
+            }
+          }
+        }
+      }
+}
