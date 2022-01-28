3cb5209cc115cb01e0099594eba72f7b588d289d
Sarang-R-119 <sarangvr@yahoo.com>
2022-01-28 13:02:12 -0500

    Modified the level1.json

--- classes/Game.js
+++ classes/Game.js
@@ -28,25 +28,25 @@ function Game() {
   this.state = 0;
 }
 
-Game.prototype.initTilemap = function () {
-  for (let i = 0; i < this.tilemap.length; i++) {
-    for (let j = 0; j < this.tilemap[i].length; j++) {
-      switch (this.tilemap[i][j]) {
-        case "w":
-          this.walls.push(new Wall(envCapture[2], j * 20, i * 20));
-          break;
+// Game.prototype.initTilemap = function () {
+//   for (let i = 0; i < this.tilemap.length; i++) {
+//     for (let j = 0; j < this.tilemap[i].length; j++) {
+//       switch (this.tilemap[i][j]) {
+//         case "w":
+//           this.walls.push(new Wall(envCapture[2], j * 20, i * 20));
+//           break;
 
-        case "t":
-          this.tiles.push(new Tile(envCapture[1], j * 20, i * 20));
-          break;
+//         case "t":
+//           this.tiles.push(new Tile(envCapture[1], j * 20, i * 20));
+//           break;
 
-        case "d":
-          this.doors = new Door(doors, j * 20, i * 20, 20, 20);
-          break;
-      }
-    }
-  }
-};
+//         case "d":
+//           this.doors = new Door(doors, j * 20, i * 20, 20, 20);
+//           break;
+//       }
+//     }
+//   }
+// };
 
 Game.prototype.drawStartScreen = function () {
   push();
